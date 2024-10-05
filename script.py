733429fcc2ee3a9c1ced3ff6ea0001433623f0a5
Lucas Ribeiro <lucas.daniel@gmail.com>
2024-10-04 23:49:26 -0400

    refactor functions

--- script.py
+++ script.py
@@ -16,8 +16,6 @@ selenium_file = "MinhasEconomiasNew.side"
 os.makedirs(output_folder, exist_ok=True)
 
 # Function to get the current timestamp in yyyymmddhhmmss format
-
-
 def get_timestamp():
     return datetime.now().strftime("%Y%m%d%H%M%S")
 
@@ -75,53 +73,7 @@ def add_to_json(command, target, value):
 
     return data
 
-# PC Bank function to process the CSV file
-def process_pc_bank(file_name):
-    input_file = os.path.join(input_folder, file_name)
-    timestamp = get_timestamp()
-    output_file = os.path.join(output_folder, f"pc_bank_output_{timestamp}.txt")
-
-    data_list = []
-    with open(input_file, mode='r', newline='', encoding='utf-8') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        
-        #Append Initial Action
-        data_list.append(add_to_json('click','css=#ext-comp-1193 .x-btn-mc',''))
-        
-        for row in csv_reader:
-            # Convert date format
-            date = convert_date_format(row['Date'])
-            
-            # Replace periods by commas
-            value = row['Amount'].replace('.', ',')
-            value_float = float(row['Amount'])
-            
-            # Description
-            description = row['Description']
-
-            data_list = data_list + add_item(date, description, value, value_float)
-            # data_list.append(add_item(date, description, value, value_float))
-            
-            # # Append the processed data to a list with the unique ID
-            # data_list.append(add_to_json('type','id=ext-comp-1814',date))
-            # data_list.append(add_to_json('type','id=ext-comp-1816',description))
-            # data_list.append(add_to_json('pause','1000',''))
-
-            # if value_float < 0:
-            #     # type expenses     
-            #     data_list.append(add_to_json('click','id=ext-comp-1884',''))
-            # else:
-            #     # type income
-            #     data_list.append(add_to_json('click','id=ext-comp-1886','Can'))
-
-            # data_list.append(add_to_json('type','id=ext-comp-1821','Can'))
-            # data_list.append(add_to_json('type','id=ext-comp-1821','Canada'))
-            # data_list.append(add_to_json('sendKeys','id=ext-comp-1821','${KEY_ENTER}'))
-            # data_list.append(add_to_json('type','id=ext-comp-1825',value))
-            # data_list.append(add_to_json('sendKeys','id=ext-comp-1825','${KEY_ENTER}'))
-            # data_list.append(add_to_json('waitForElementVisible',"xpath=//*[contains(text(), 'adicionada com sucesso')]",'30000'))
-            # data_list.append(add_to_json('waitForElementNotVisible',"xpath=//*[contains(text(), 'adicionada com sucesso')]",'80000'))
-            
+def handler_output_files(data_list, output_file):
     # Serializing json
     # json_object = json.dumps(data_list, indent=4)
 
@@ -151,6 +103,38 @@ def process_pc_bank(file_name):
     destination_file =  os.path.join(selenium_folder, selenium_file)
     with open(destination_file, 'w') as dest:
         dest.write(modified_content)
+
+    return destination_file
+
+# PC Bank function to process the CSV file
+def process_pc_bank(file_name):
+    input_file = os.path.join(input_folder, file_name)
+    timestamp = get_timestamp()
+    output_file = os.path.join(output_folder, f"pc_bank_output_{timestamp}.txt")
+
+    data_list = []
+    with open(input_file, mode='r', newline='', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        
+        #Append Initial Action
+        data_list.append(add_to_json('click','css=#ext-comp-1193 .x-btn-mc',''))
+        
+        for row in csv_reader:
+            # Convert date format
+            date = convert_date_format(row['Date'])
+            
+            # Replace periods by commas
+            value = row['Amount'].replace('.', ',')
+            value_float = float(row['Amount'])
+            
+            # Description
+            description = row['Description']
+
+            # add item        
+            data_list = data_list + add_item(date, description, value, value_float)
+
+    # handler files
+    destination_file = handler_output_files(data_list, output_file)
 
     print(f"PC Bank data successfully processed and saved to {output_file}")
     print(f"Selenium file successfully created to  {destination_file}")
